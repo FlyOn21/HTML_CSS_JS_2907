179c1491775ca86c869ff8fb8fe7bc37de2d507c
flyon21 <zhogolevpv@gmail.com>
2021-08-26 20:26:28 +0300

    fix

--- JS_lesson_7/cockroachApocalypse.js
+++ JS_lesson_7/cockroachApocalypse.js
@@ -1,10 +1,20 @@
 function cockroachApocalypse (speed, slowdown, mine) {
-    const distancePerHour  = speed - slowdown;
-    const result = mine/distancePerHour
-    console.log(result)
-    return result
+    let time = 0;
+    let height = 0;
+    do{
+        time++;
+        height+= speed;
+        if(height<mine){
+            height-=slowdown;
+        }
+    }
+     while (height<=mine){
+        time -=(height-mine)/speed
+    }
+    console.log(time)
+    return time
 }
 
 
-cockroachApocalypse(1,0.5,5)
+cockroachApocalypse(1,0.5,3)
 cockroachApocalypse(3.6,2.1,3.5)
